7e9485aa13e244654d193004aaa2bfedee3d4393
Alexis <alexis.fouquet@bts-malraux.net>
2024-04-10 15:30:19 +0200

    update

--- elements/php/signup.php
+++ elements/php/signup.php
@@ -1,18 +1,33 @@
-<?php $host = '192.168.3.211';  // host = etu.bts-malraux72.net ou 192.168.3.211
+<?php         
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST['name'])){
+            $erreurName = "Veuillez saisir un Nom d'utilisateur";
+        }
+        if (empty($_POST['email'])){
+            $erreurEmail = "Veuillez saisir une Adresse mail";
+        }
+        if (empty($_POST['password'])){
+            $erreurMdp = "Veuillez saisir un Mot de passe";
+        }
+        else if (!empty($_POST['password']) && !empty($_POST['email']) && !empty($_POST['password']))
+            {
+$host = '192.168.3.211';  // host = etu.bts-malraux72.net ou 192.168.3.211
 $dbname = 'a.fouquet';
 $username = 'a.fouquet';
 $password = 'P@ssword';
 
     $dbh = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
         $username = $_POST['name'];
         $email = $_POST['email'];
         $password = $_POST['password'];
 
-$sql = "INSERT INTO semireves.utilisateurs (username, email, pwd, rank) VALUES ('$username', '$email', '$password', 'Utilisateur')";
-$stmt = $dbh->prepare($sql);
+        $sql = "INSERT INTO semireves.utilisateurs (username, email, pwd, rank) VALUES ('$username', '$email', '$password', 'Utilisateur')";
+        $stmt = $dbh->prepare($sql);
 
         $stmt->execute();
+        header('LOCATION: index.php');
+        }
     }
 ?>
